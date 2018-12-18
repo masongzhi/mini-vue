69186a896482a7b83f12541ad378507dcbc1c47d
masongzhi <511071161@qq.com>
2018-12-18 18:33:40 +0800

    删除get函数

--- src/instance.js
+++ src/instance.js
@@ -1,16 +1,6 @@
 import { handleData, handleWatchers, sub } from "./observe";
 import set from 'lodash/set'
 
-function get(object, key) {
-  let newObject = object
-  if (/\./.test(key)) {
-    key.split('.').forEach(it => {
-      newObject = newObject[it]
-    })
-  }
-  return newObject
-}
-
 export default class Vue {
   constructor(config) {
     this.$options = config;
